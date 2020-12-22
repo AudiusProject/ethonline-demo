8a22e5db4c6d93f80e83d23171136bf4f58eebe6
Raymond Jacobson <ray@audius.co>
2020-12-21 16:49:05 -0800

    Update values

--- src/examples/LibsExample/LibsExample.jsx
+++ src/examples/LibsExample/LibsExample.jsx
@@ -9,10 +9,10 @@ import './LibsExample.css'
 const init = async () => {
   const dataRegistryAddress = '0xC611C82150b56E6e4Ec5973AcAbA8835Dd0d75A2'
 
-  const ethTokenAddress = '0xADEf65C0f6a30Dcb5f88Eb8653BBFe09Bf99864f'
-  const ethRegistryAddress = '0xb2be26Ca062c5D74964921B80DE6cfa28D9A36c0'
+  const ethTokenAddress = '0x18aAA7115705e8be94bfFEBDE57Af9BFc265B998'
+  const ethRegistryAddress = '0xd976d3b4f4e22a238c1A736b6612D22f17b6f64C'
   const ethProviderUrl = 'https://mainnet.infura.io/v3/d6b566d7eea1408988388c311d5a273a'
-  const ethProviderOwnerWallet = '0xe886a1858d2d368ef8f02c65bdd470396a1ab188'
+  const ethProviderOwnerWallet = '0xC7310a03e930DD659E15305ed7e1F5Df0F0426C5'
 
   const libs = new Audius({
     web3Config: Audius.configInternalWeb3(
